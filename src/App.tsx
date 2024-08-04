1b7279f291315ec3c99b492ff1af3695e3212f81
Kamil <kamil.walczak@cobrick.com>
2024-08-04 11:04:10 +0200

    Creating site under construction

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="logo" alt="logo" />
+      <h3>Strona w budowie</h3>
+
+      <h4>Kontakt:</h4>
+      <p>biuro@taupe-interior.pl</p>
+      <p>tel.: 570 750 870 </p>
+    </div>
+  );
+}
+
+export default App;
