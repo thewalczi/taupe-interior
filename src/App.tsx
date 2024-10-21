50d878ae1090999c1a1cccc43d8a0ea45ad3e750
Kamil <kamil.walczak@cobrick.com>
2024-10-21 20:32:53 +0200

    Add projects page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import { Header } from './components/header/Header';
 import { HomePage } from './pages/Home/Home.page';
 import { mediaQuery } from './helpers/breakpoints';
+import { ProjectsPage } from './pages/Projects.page';
 
 function App() {
   const CSSVariables = styled.div`
+    --color-primary: #a1988f;
     --headerPaddingVertical: 1.6rem;
     --headerPaddingHorizontal: 1.6rem;
     --headerLogoSize: 8rem;
@@ -17,6 +19,7 @@ function App() {
     <CSSVariables>
       <Header />
       <HomePage />
+      <ProjectsPage />
       <div style={{ width: '100%', height: '800px', backgroundColor: '#efefef' }}>section2</div>
       <div style={{ width: '100%', height: '800px', backgroundColor: 'lightblue' }}>section2</div>
     </CSSVariables>
