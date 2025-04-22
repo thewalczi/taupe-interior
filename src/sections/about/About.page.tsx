a54a3ff9c42334a8b006d3efc0cfd5d134e248a3
Kamil <kamil.walczak@cobrick.com>
2025-04-22 21:17:49 +0200

    Test actions

--- src/sections/about/About.page.tsx
+++ src/sections/about/About.page.tsx
@@ -6,11 +6,12 @@ export const AboutPage = () => {
       <div className={styles.wrapper}>
         <div className={styles.avatar} />
         <p className={styles.text}>
-          Duis at mi non tellus malesuada tincidunt. Cras in neque. Sed lacinia, felis ut sodales pretium, justo sapien
+          Test
+          {/* Duis at mi non tellus malesuada tincidunt. Cras in neque. Sed lacinia, felis ut sodales pretium, justo sapien
           hendrerit est, et convallis nisi quam sit amet erat. Suspendisse consequat nibh a mauris. Curabitur libero
           ligula, faucibus at, mollis ornare, mattis et, libero. <br /> Aliquam pulvinar congue pede. Fusce condimentum
           turpis vel dolor. Ut blandit. Sed elementum justo quis sem. Sed eu orci eu ante iaculis accumsan. Sed suscipit
-          dolor quis mi. Curabitur ultrices nonummy lacus. Morbi ipsum ipsum, adipiscing eget, tincidunt{' '}
+          dolor quis mi. Curabitur ultrices nonummy lacus. Morbi ipsum ipsum, adipiscing eget, tincidunt{' '} */}
         </p>
       </div>
     </section>
