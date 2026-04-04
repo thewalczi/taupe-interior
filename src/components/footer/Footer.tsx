94dc411c01f99c50687f804c35b8b4a46c439227
Kamil Walczak <kamilwalczak@MacBook-Air-Kamil.local>
2026-04-04 18:29:53 +0200

    Make footer year to self updates, decrease logo size

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
+import { useMemo } from 'react';
 import { Socials } from '../socials/Socials';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
+  const currentYear = useMemo(() => new Date().getFullYear(), []);
   return (
     <footer className={styles.container}>
       <Socials />
-      <p>© Taupe Interior 2025</p>
+      <p>© Taupe Interior {currentYear}</p>
     </footer>
   );
 };
